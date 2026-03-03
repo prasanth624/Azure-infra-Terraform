f3b46764205b13303e2197d9b571eb20b46d5967
Prasanth B <125499600+prasanth624@users.noreply.github.com>
2026-03-04 02:40:54 +0530

    Create providers.tf

---
+++ environments/dev/providers.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+    }
+  }
+
+  # Optional: set subscription
+  # subscription_id = var.subscription_id
+}
